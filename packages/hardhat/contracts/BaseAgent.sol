c86e8fa6d61188eed34da060d39f3299be2af159
Gonzalo Melo <gonzalomelov@gmail.com>
2024-12-14 22:46:44 -0300

    Remove unused contracts

--- packages/hardhat/contracts/BaseAgent.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-import "./EnhancedOpenAiChatGpt.sol";
-
-contract BaseAgent is EnhancedOpenAiChatGpt {
-    constructor(address initialOracleAddress) EnhancedOpenAiChatGpt(initialOracleAddress) {}
-}
--- packages/hardhat/contracts/DataAgent.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-import "./BaseAgent.sol";
-
-contract DataAgent is BaseAgent {
-    constructor(address initialOracleAddress) BaseAgent(initialOracleAddress) {}
-}
